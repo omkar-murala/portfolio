f37c4f4a53e016ce7a9b9285aad8940a8babdd1f
omkar-murala <omkar.developer1579@gmail.com>
2025-06-09 19:38:31 +0530

    Initial commit: frontend and backend

---
+++ backend/Portfolio-backend/src/main/java/com/omkar/Portfolio_backend/PortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omkar.Portfolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+}
